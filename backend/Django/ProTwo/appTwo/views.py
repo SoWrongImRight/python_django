2ddf90cc2b5e7f56d67a1d677972ac5530e0a6d9
RUss Carroll <russ.carroll@pm.me>
2020-11-12 15:46:03 -0500

    added Model Forms

--- backend/Django/ProTwo/appTwo/views.py
+++ backend/Django/ProTwo/appTwo/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from appTwo.models import User
+from appTwo.forms import NewUserForm
 
 # Create your views here.
 def index(request):
@@ -9,7 +8,17 @@ def index(request):
 
 
 def users(request):
+   form = NewUserForm()
 
-    user_list = User.objects.order_by('first_name')
-    user_dict = {"users":user_list}
-    return render(request,'appTwo/users.html',context=user_dict)
+   if request.method == 'POST':
+      form = NewUserForm(request.POST)
+   
+   if form.is_valid():
+      form.save(commit=True)
+      return index(request)
+   
+   else:
+      print("ERROR form invalid")
+
+   return render(request,'appTwo/users.html',{'form':form})
+   
