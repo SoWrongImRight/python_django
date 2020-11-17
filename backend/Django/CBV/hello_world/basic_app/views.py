d86dde4cb43b0b2f1186532d1e0ec1f6c18a1131
SoWrongImRight <58298347+SoWrongImRight@users.noreply.github.com>
2020-11-17 07:31:39 -0500

    Adding files for two new class views

--- backend/Django/CBV/hello_world/basic_app/views.py
+++ backend/Django/CBV/hello_world/basic_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, ListView, DetailView
+from basic_app.models import models
 
 
 # Create your views here.
-class IndexView(TemplateView):
-    template_name = 'basic_app/index.html'
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION'
-        return context
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name='basic_app/school_detail.html'
    
