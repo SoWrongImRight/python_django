5bf1d62cd8d50570d8c3dfb8449bb1c4c476339a
SoWrongImRight <58298347+SoWrongImRight@users.noreply.github.com>
2020-11-17 21:48:01 -0500

    Updates

--- backend/Django/CBV/hello_world/basic_app/views.py
+++ backend/Django/CBV/hello_world/basic_app/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView, DetailView
-from basic_app.models import models
+
 
 
 # Create your views here.
 class SchoolListView(ListView):
-    context_object_name = 'schools'
+    # If you don't pass in this attribute,
+    # Django will auto create a context name
+    # for you with object_list!
+    # Default would be 'school_list'
+
+    # Example of making your own:
+    # context_object_name = 'schools'
     model = models.School
 
+
 class SchoolDetailView(DetailView):
-    context_object_name = 'school_detail'
+    context_object_name = 'school_details'
     model = models.School
-    template_name='basic_app/school_detail.html'
-   
+    template_name = 'basic_app/school_detail.html'
