4c776d588e367dc7f1a4b9658f22a98955de7337
SoWrongImRight <58298347+SoWrongImRight@users.noreply.github.com>
2021-06-12 20:39:08 -0400

    Updated forms, models, and settings.

--- blog_project/myblog/blog/models.py
+++ blog_project/myblog/blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
   def __str__(self):
     return self.title
 
-class Comments(models.Model):
+class Comment(models.Model):
   post = models.ForeignKey('blog.Post', related_name='comments')
   author = models.CharField('max_lenght=200')
   text = models.TextField()
